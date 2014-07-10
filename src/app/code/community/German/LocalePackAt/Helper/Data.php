f792a20468af3bbb2c9ab66c53233daf5e788e5f
MaWoScha <manfredwormschaefer@gmail.com>
2014-07-10 21:24:43 +0200

    Uebersetzungsfehler im Downloader behoben. Fehlende Zeile wieder eingefuegt in Datei downloader/template_de_1.x/connect/packages.phtml. Lokalisierung des Sprachpakets de_DE: Deutsch, Englisch und Spanisch sind nun verfuegbar.

--- src/app/code/community/German/LocalePackAt/Helper/Data.php
+++ src/app/code/community/German/LocalePackAt/Helper/Data.php
@@ -4,10 +4,10 @@
  * @package German_LocalePack
  * @authors Rico Neitzel <rico@buro71a.de, http://www.buro71a.de/>
  * @developer Rico Neitzel <rico@buro71a.de, http://www.buro71a.de/>
- * @version 1.9.0.1.0
+ * @version 1.9.0.0.0
  * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class German_LocalePackDe_Helper_Data extends Mage_Core_Helper_Abstract
+class German_LocalePackAt_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
 }
